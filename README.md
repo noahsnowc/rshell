3b989ef5c906341b82f57d7777e38c55da1810b9
noahcz <noahsnowc@gmail.com>
2022-03-14 20:18:35 +0000

    CD and Exit work

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # rshell
 A simple shell built in Rust 
+
+Uses https://github.com/JoshMcguigan/bubble-shell as a starting point
+
+## What's Working:
+- [x] "cd"
+- [x] "exit"
+- [ ] Pipes
+- [ ] Current WD
+
+## License
+
+Licensed under either of
+
+* Apache License, Version 2.0, (LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0)
+* MIT license (LICENSE-MIT or http://opensource.org/licenses/MIT)
+
+at your option.
+
+## Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
--- main.rs
+++ main.rs
@@ -1,1 +1,46 @@
+use std::env;
+use std::io::{stdin, stdout, Write};
+use std::path::Path;
+use std::process::{Child, Command, Stdio}; 
 
+fn main(){
+    loop{
+        print!("> ");
+        stdout().flush();
+
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+        
+
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+
+        let args = parts;
+        match command{
+            "cd" =>{
+                //default to '/' as new directory if one was not provided
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            },
+            "exit" => return,
+            command =>{
+                let mut child = Command::new(command)
+                .args(args)
+                .spawn();
+    
+                //don't accept another command until this one completes.
+                match child {
+                    Ok(mut child) => {child.wait();},
+                    Err(e) => eprintln!("{}", e),
+                };
+            }
+            
+        }
+        
+    }
+      
+}
